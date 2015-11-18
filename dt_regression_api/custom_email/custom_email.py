32eba74d66d4427e0eb036bbc36ea7d024b1a5c7
Alan Dinneen <adinneen@eshots.com>
2015-11-18 15:58:16 -0600

    Updated email to string format successful message.

--- dt_regression_api/custom_email/custom_email.py
+++ dt_regression_api/custom_email/custom_email.py
@@ -14,7 +14,7 @@ class Email(object):
     _MSGSUBJECT = 'DT Testing API -- Response'
     _BASE_EMAIL_HTML = """
         <html>
-            <head>DT Regression API</head>
+            <head>DT Regression API Response Message</head>
             <body>
                 <p>
                     {0}
